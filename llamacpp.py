819d70665390742c1af26dc2b3141f8f50c70ffb
AdrianVillegas <adrianvillegasj@gmail.com>
2023-07-14 03:50:01 +0900

    LLMChain

--- llamacpp.py
+++ llamacpp.py
@@ -1,7 +1,22 @@
 from llama_cpp import Llama
+from langchain import PromptTemplate, LLMChain
+
 
 llm = Llama(model_path="./wizardLM-7B.ggmlv3.q8_0.bin")
+template = """Question: {question}
+
+Answer: Let's work this out in a step by step way to be sure we have the right answer."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
+
+llm_chain.run(question)
+
 inputdata = ""
+json_fields = ["zip_code", "work_tech", "company"]
 
 while True:
     inputdata = input("Input : ")
@@ -12,10 +27,11 @@ while True:
         + "\n Person B: "
         + answer
         + "' \n In above chat history, "
-        + inputdata
         + " A: ",
         max_tokens=64,
         stop=["Q:", "\n"],
         echo=True,
     )
     print(output)
+
+    # What is the zip code in Person B's answer in a word?
