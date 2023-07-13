d7abd6d5c833f56848147e58bfdbf2074c2e5f60
AdrianVillegas <adrianvillegasj@gmail.com>
2023-07-14 07:15:32 +0900

    231321

--- llamacpp.py
+++ llamacpp.py
@@ -30,7 +30,7 @@ while True:
             + " "
             + answer
             + "' \n "
-            + " What is the above content's answer in a word? If you are not sure about above chat's answer, return 'None'. A: ",
+            + " What is the above chat history's answer in a word? If you are not sure about above chat's answer, return 'None'. A: ",
             max_tokens=64,
             # stop=["Q:", "\n"],
             echo=True,
