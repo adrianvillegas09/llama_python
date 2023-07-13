790b465ef60b5c7b040cef4cb9e927c617798aae
AdrianVillegas <adrianvillegasj@gmail.com>
2023-07-14 01:21:18 +0900

    updated template

--- lang.py
+++ lang.py
@@ -1,37 +1,39 @@
-from langchain import PromptTemplate
+from langchain import LlamaCpp, PromptTemplate
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferWindowMemory
-
-from langchain.llms import LlamaCpp
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
+# template = """Below is the chat history.
+# If you can't reply the answer in a word similarily, reply 'None'.
+# I want you to answer the correct answer in the following text in a word.
+# 'A: What is your zip code?
+#  B:No, why?'"""
 
-# callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+# prompt = PromptTemplate.from_template(template)
 
-llm = LlamaCpp(
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+local_llm = LlamaCpp(
     model_path="./wizardLM-7B.ggmlv3.q8_0.bin",
-    # callback_manager=callback_manager,
+    callback_manager=callback_manager,
     verbose=True,
 )
 
-template = """Below is the chat history. 
-I want you to answer the correct answer in the above content in a word. If you can't reply the answer in a word similarily, reply 'None'.
-'A: What is your zip code?
- B:No, why?'"""
+summarize_template_string = """
+        Provide a summary for the following text:
+        {text}
+"""
 
-prompt = PromptTemplate.from_template(template)
-
-chatgpt_chain = LLMChain(
-    llm=llm,
-    prompt=prompt,
-    verbose=False,
-    memory=ConversationBufferWindowMemory(k=2),
+summarize_prompt = PromptTemplate(
+    template=summarize_template_string,
+    input_variables=["text"],
 )
 
-print("GratitudeLLM loading...")
+summarize_chain = LLMChain(
+    llm=local_llm,
+    prompt=summarize_prompt,
+)
 
-# human_input = input("\nHuman: ")
-output = chatgpt_chain.predict()
-print(output)
-print("Done!")
+text = input("Input : ")
+summary = summarize_chain.run(text=text)
+print(summary)
