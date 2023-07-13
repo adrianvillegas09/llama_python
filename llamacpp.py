a7c7f2f29864d406f867b91af85fda57cca241e7
AdrianVillegas <adrianvillegasj@gmail.com>
2023-07-14 00:25:12 +0900

    updated module

--- llamacpp.py
+++ llamacpp.py
@@ -2,10 +2,10 @@ from llama_cpp import Llama
 
 llm = Llama(model_path="./wizardLM-7B.ggmlv3.q8_0.bin")
 output = llm(
-    """Below is the chat history. 
+    """Q: Below is the chat history. 
 I want you to answer the correct answer in the above content in a word. If you can't reply the answer in a word similarily, reply 'None'.
 'A: What is your zip code?
- B:No, why?'""",
+ B:No, why?' A: """,
     max_tokens=64,
     stop=["Q:", "\n"],
     echo=True,
