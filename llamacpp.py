31a716a7a2029069144dc90ef6ef7ad4b8552802
AdrianVillegas <adrianvillegasj@gmail.com>
2023-07-14 02:57:49 +0900

    none1

--- llamacpp.py
+++ llamacpp.py
@@ -8,7 +8,7 @@ while True:
     output = llm(
         "Q: 'Person A: "
         + inputdata
-        + "\n Person B: No, why?' In above chat history, Did you find the Person B's info in a word? If you find the Person B's info in a word, reply 'None'"
+        + "\n Person B: No, why?' In above chat history, Did you find the Person B's info in a word? If you can't find the Person B's info in a word, answer 'None'"
         + " A: ",
         max_tokens=64,
         stop=["Q:", "\n"],
