b7a4d817c5fed76b0fc77f3ad891a928908847f5
adrianvillegasj@gmail.com <adrianvillegasj@gmail.com>
2023-09-23 05:35:05 +0900

    langchain

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
 
-# Load pre-trained model (weights)
-model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-13b-v1.3")
+model = "tiiuae/falcon-7b-instruct"
 
-# Load pre-trained model tokenizer
-tokenizer = AutoTokenizer.from_pretrained("lmsys/vicuna-13b-v1.3")
-
-# Make sure all model parameters do not require gradients
-for param in model.parameters():
-    param.requires_grad = False
-
-# Conversation pipeline
-conversational_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
-
-while True:
-    # Get user input
-    user_input = input("User: Hi!")
-
-    # Generate response
-    model_input = tokenizer.encode(
-        user_input + tokenizer.eos_token, return_tensors="pt"
-    )
-    response_tokenized = model.generate(
-        model_input, max_length=1000, pad_token_id=tokenizer.eos_token_id
-    )
-    response = tokenizer.decode(
-        response_tokenized[:, model_input.shape[-1] :][0], skip_special_tokens=True
-    )
-
-    print("Bot: ", response)
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
+sequences = pipeline(
+    "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
+    max_length=200,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
