2b6bf006fd794749a75f0eca143ff24b54512d75
adrianvillegasj@gmail.com <adrianvillegasj@gmail.com>
2023-09-23 05:42:08 +0900

    langchain

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ pipeline = transformers.pipeline(
     device_map="cuda:0",
 )
 sequences = pipeline(
-    "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
+    "Hello",
     max_length=200,
     do_sample=True,
     top_k=10,
