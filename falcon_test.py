e31654d5b06a74c8713e8fc6e72667f408aaa0ca
adrianvillegasj@gmail.com <adrianvillegasj@gmail.com>
2023-09-23 08:59:08 +0900

    lang

--- falcon_test.py
+++ falcon_test.py
@@ -25,18 +25,18 @@ pipeline = transformers.pipeline(
 )
 llm = HuggingFacePipeline(pipeline=pipeline)
 
-tokenizer_llama = AutoTokenizer.from_pretrained(model_llama)
-pipeline_llama = transformers.pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    max_length=700,
-    temperature=0,
-    trust_remote_code=True,
-    device_map="cuda:0",
-)
-llm_llama = HuggingFacePipeline(pipeline=pipeline_llama)
+# tokenizer_llama = AutoTokenizer.from_pretrained(model_llama)
+# pipeline_llama = transformers.pipeline(
+#     "text-generation",
+#     model=model,
+#     tokenizer=tokenizer,
+#     torch_dtype=torch.bfloat16,
+#     max_length=700,
+#     temperature=0,
+#     trust_remote_code=True,
+#     device_map="cuda:0",
+# )
+# llm_llama = HuggingFacePipeline(pipeline=pipeline_llama)
 
 app = Flask(__name__)
 CORS(app)
