b6979472821baa313e901305ab517a7b9fa27962
adrianvillegasj@gmail.com <adrianvillegasj@gmail.com>
2023-09-23 09:15:32 +0900

    lang

--- falcon_test.py
+++ falcon_test.py
@@ -12,7 +12,7 @@ print(torch.cuda.is_available())
 model = "tiiuae/falcon-7b-instruct"
 model_llama = "NousResearch/Llama-2-7b-hf"
 
-tokenizer = AutoTokenizer.from_pretrained(model_llama)
+tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
