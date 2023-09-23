e4c1345ec231cd86b5d1b6fa0a372067a643f56f
adrianvillegasj@gmail.com <adrianvillegasj@gmail.com>
2023-09-23 10:44:41 +0900

    flow

--- llama_test.py
+++ llama_test.py
@@ -1,17 +1,13 @@
-from transformers import AutoTokenizer
-import transformers
 import torch
-from langchain.llms import HuggingFacePipeline
-from langchain import PromptTemplate, LLMChain
 
-# from llama_cpp import Llama
+from llama_cpp import Llama
 
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 print(torch.cuda.is_available())
 
-llm_llama = Llama(model_path="llama-2-7b-chat.ggmlv3.q2_K.bin.bin")
+llm = Llama(model_path="./llama-2-7b-chat.ggmlv3.q2_K.bin")
 
 app = Flask(__name__)
 CORS(app)
@@ -24,20 +20,23 @@ def query():
     and have the ability to insert opinion on current affairs, media, trends, and general social commentary
     when prompted. You will understand specific humor based off pop culture and media, sarcasm,
     and social references.
-    Q: {query}
-    A:"""
+    Q: """
 
     query_data = request.get_json()
     query = query_data["query"]
     model_type = query_data["model_type"]
 
-    prompt = PromptTemplate(template=template, input_variables=["query"])
+    prompt = template + query + "A: "
 
-    # chain
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
-
-    response = llm_chain.run(query)
-    return {"message": response}
+    response = llm(
+        prompt,
+        max_tokens=200,
+        stop=["Q:", "\n"],
+        echo=True,
+    )
+    answer_point = response["choices"][0]["text"].find("A: ")
+    sub_answer = response["choices"][0]["text"][answer_point:]
+    return {"message": sub_answer}
 
 
 if __name__ == "__main__":
