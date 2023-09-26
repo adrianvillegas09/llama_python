1bd8d94bb1eba40e505e6ffb7fda568f163dc64e
adrianvillegasj@gmail.com <adrianvillegasj@gmail.com>
2023-09-26 22:06:02 +0900

    falcon module updated

--- falcon_test.py
+++ falcon_test.py
@@ -8,10 +8,18 @@ from langchain import PromptTemplate, LLMChain
 
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+from threading import Thread
 
 print(torch.cuda.is_available())
 
 model = "tiiuae/falcon-7b-instruct"
+template = """
+You are an intelligent chatbot that can function as a brand copywriter, customer service manager,
+and have the ability to insert opinion on current affairs, media, trends, and general social commentary
+when prompted. You will understand specific humor based off pop culture and media, sarcasm,
+and social references.
+Question: {query}
+Answer:"""
 
 tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = transformers.pipeline(
@@ -26,7 +34,10 @@ pipeline = transformers.pipeline(
 )
 llm = HuggingFacePipeline(pipeline=pipeline)
 
-# llm_llama = Llama(model_path="llama-2-7b-chat.ggmlv3.q2_K.bin.bin")
+prompt = PromptTemplate(template=template, input_variables=["query"])
+
+# chain
+llm_chain = LLMChain(prompt=prompt, llm=llm)
 
 app = Flask(__name__)
 CORS(app)
@@ -34,26 +45,13 @@ CORS(app)
 
 @app.route("/api/query", methods=["POST"])
 def query():
-    template = """
-    You are an intelligent chatbot that can function as a brand copywriter, customer service manager,
-    and have the ability to insert opinion on current affairs, media, trends, and general social commentary
-    when prompted. You will understand specific humor based off pop culture and media, sarcasm,
-    and social references.
-    Question: {query}
-    Answer:"""
-
     query_data = request.get_json()
     query = query_data["query"]
     model_type = query_data["model_type"]
-
-    prompt = PromptTemplate(template=template, input_variables=["query"])
-
-    # chain
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
 
     response = llm_chain.run(query)
     return {"message": response}
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", 8000)
+    app.run("0.0.0.0", 8000, debug=False)
